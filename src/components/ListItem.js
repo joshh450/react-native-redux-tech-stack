e993ae91d2597b83fc99124830d5ac29b1235290
joshh450 <joshh450@yahoo.com>
2018-12-11 15:31:30 -0700

    working on connecting components to redux store

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
-
+import * as actions from '../actions';
+ 
 class ListItem extends Component {
+    renderDescription() {
+        const { library, selectedLibraryId } = this.props;
+ 
+        if (library.item.id === selectedLibraryId) {
+            return (
+                <Text>{library.item.description}</Text>
+            );
+        }
+    }
+ 
+ 
     render() {
         const { titleStyle } = styles;
-
-        return(
-            <CardSection>
-                <Text style={titleStyle}>
-                    {this.props.library.item.title} 
-                </Text>
-            </CardSection>
-        );
+        const { id, title } = this.props.library.item;
+ 
+        return (
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id) }
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
+        )
     }
 }
-
+ 
 const styles = {
     titleStyle: {
         fontSize: 18,
         paddingLeft: 15
     }
-}
-
-export default ListItem;
+};
+ 
+const mapStateToProps = state => {
+    return { selectedLibraryId: state.selectedLibraryId };
+};
+ 
+export default connect(mapStateToProps, actions)(ListItem);
